2b46aa1448103d20d4e3e90387ddb5d837d05f58
Vikas Kapadiya <vikas@kapadiya.net>
2025-11-01 04:41:44 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { WorkerEntrypoint } from 'cloudflare:workers';
 import * as GooglePhotosAlbum from 'google-photos-album-image-url-fetch';
 
-export default class WorkerB extends WorkerEntrypoint {
+export default {
 	// Currently, entrypoints without a named handler are not supported
 	async fetch(request: Request) {
-		const albumUrl = request.url;
+		//const albumUrl = request.url;
 
-		if (!albumUrl) {
-			return new Response(
-				JSON.stringify({ error: 'albumUrl query parameter is required' }),
-				{
-					status: 400,
-					headers: { 'Content-Type': 'application/json' },
-				},
-			);
+		const url = new URL(request.url);
+
+		if(!url.href.startsWith("https://photos.app.goo.gl/")) {
+			return new Response(JSON.stringify({ error: 'Invalid request' }), {
+				status: 200,
+				headers: { 'Content-Type': 'application/json' },
+			});
 		}
 
+		const albumUrl = url.href;
+
+		//const albumUrl = 'https://photos.app.goo.gl/h2Jwy1115xj3W8xD9';
+
 		const re = await GooglePhotosAlbum.fetchImageUrls(albumUrl);
-		console.log(JSON.stringify(re, null, 2));
+		//console.log(JSON.stringify(re, null, 2));
 		return new Response(JSON.stringify(re), {
 			status: 200,
 			headers: { 'Content-Type': 'application/json' },
 		});
-	}
-}
+	},
+};
